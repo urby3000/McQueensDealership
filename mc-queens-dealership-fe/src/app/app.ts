4673855d5d23b764b05058cb9996a1216ed46808
me <urbanbozic1996@gmail.com>
2026-01-30 18:31:09 +0100

    drew plans for DB and UI :) and started with home page UI

--- mc-queens-dealership-fe/src/app/app.ts
+++ mc-queens-dealership-fe/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
