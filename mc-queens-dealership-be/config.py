e55afbaec0d06f3acc5b05bf638062d0e0bd51cb
me <urbanbozic1996@gmail.com>
2026-02-04 14:56:55 +0100

    database on mysql

--- mc-queens-dealership-be/config.py
+++ mc-queens-dealership-be/config.py
@@ -1,5 +1,6 @@
 config = {
-    "SQLALCHEMY_DATABASE_URI" : "sqlite:///project.db",
+    # "SQLALCHEMY_DATABASE_URI" : "sqlite:///project.db",
+    "SQLALCHEMY_DATABASE_URI" : "mysql+pymysql://root:admin@localhost:3306/mc-queens-dealership",
     "SQLALCHEMY_TRACK_MODIFICATIONS" : False,
     "SECRET_KEY" : "supersecretkey",
     "UPLOAD_EXTENSIONS" : [".jpg", ".png"],
